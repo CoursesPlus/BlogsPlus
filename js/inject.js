8b4df84b39628aa4356eb8a47bf5d3efb03d13ce
eeshan tripathii <eeshan.tripathii@gmail.com>
2015-03-13 09:54:50 -0400

    Added calendar

--- js/inject.js
+++ js/inject.js
@@ -1,3 +1,108 @@
-$(document).ready(function() {
+function getMonthLength(month, year) {
+	var testDate = new Date(year, month - 1, 0);
+	return testDate.getDate();
+}
+function getStartingDayOfWeek(month, year) {
+	var testDate = new Date(year, month - 1, 1);
+	return testDate.getDay();
+}
+function weekCount(year, month_number) {
+    var firstOfMonth = new Date(year, month_number-1, 1);
+    var lastOfMonth = new Date(year, month_number, 0);
+
+    var used = firstOfMonth.getDay() + lastOfMonth.getDate();
+
+    return Math.ceil( used / 7);
+}
+function getCalendarHTML(month, year) {
+	var html = "";
+
+	var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	// var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+	html += '<table class="calendar">';
+		html += '<thead>';
+
+			html += '<tr><th class="calendar-month" colspan="7">';
+			html += months[month - 1];
+			html += ' ';
+			html += year;
+			html += '</th></tr>';
+			html += '<tr class="calendar-daysOfWeek">';
+				for (var i = 0; i < daysOfWeek.length; i++) {
+					html += '<th class="calendar-dayOfWeek">';
+					html += daysOfWeek[i];
+					html += '</th>';
+				}
+			html += '</tr>';
+
+		html += '</thead>';
+		html += '<tbody>';
+
+		// For every week...
+		// if start week, add blank cells
+		// add cells until end of week
+		// if end week, add blank cells
+		// repeat until end of month
+		var weeks = weekCount(year, month);
+		var curDay = 1;
+		for (var j = 0; j < weeks; j++) {
+			var weekDayIndex = 0;
+			var daysInWeek = 7;
+			if (j == (weeks - 1)) {
+				// Last week!						
+				var lastDay = new Date(year, month, 0);
+				daysInWeek = lastDay.getDay() + 1;
+			}
+			html += '<tr>';
+
+				if (j == 0) {
+					// Start week!
+					// We have to worry about the starting days...
+					var startDow = getStartingDayOfWeek(month,year);
+					for (var k = 0; k < startDow; k++) {
+						html += '<td class="calendar-notInMonth"></td>';
+						weekDayIndex++;
+					}
+				}
+				for (;weekDayIndex < daysInWeek; weekDayIndex++) {
+					html += '<td class="calendar-day">';
+					html += curDay;
+					html += '</td>';
+					curDay++;
+				}
+				if (weekDayIndex != 6) {
+					for (;weekDayIndex < 7; weekDayIndex++) {
+						html += '<td class="calendar-notInMonth"></td>';
+						curDay++;
+					}
+				}
+
+
+			html += '</tr>';
+		}
+
+		html += '</tbody>';
+	html += '</table>';
 	
+	return html;
+}
+
+$(document).ready(function() {
+	if (window.location.href.replace("http://", "").replace("https://", "") == "blogs.dalton.org/mines/") {
+		$.get("https://blogs.dalton.org/mines/feed/?cat=195058", function(data) {
+			var $data = $(data);
+			$data.find("item").each(function() {
+				var thisEntryDate = new Date($(this).children("pubDate").text());
+				console.log($(this).children("title").text());
+				console.log(thisEntryDate.toDateString());
+				console.log($(this).children("link").text());
+				if(thisEntryDate.getMonth() == 2) {
+					console.log("MARCH");
+				}
+			});
+			$("body").append(getCalendarHTML(3, 2015));
+		});
+	}
 });
